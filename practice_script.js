4cfc0b2fa6349324a674ad0dc96f743533650bba
JD Nafziger <nafziger10@gmail.com>
2023-08-16 17:25:17 -0500

    updated script.js to practice_script.js
    This change should make it easier to tell files apart in closed tabs when navigating between a current project and the practice page.

--- practice_script.js
+++ practice_script.js
@@ -1,7 +1,6 @@
 console.log("Welcome to my JS testing page")
 
 let matrix = [[1,2,3],[4,5,6],[7,8,9]]
-
 function transposeMatrix(matrix) {
   // Write your code here.
   let output;
@@ -9,27 +8,20 @@ function transposeMatrix(matrix) {
   // console.log(output);
   return output;
 }
-transposeMatrix(matrix);
-
-
-
-let myInt = 6;
-let myString = '6';
-let combo = myInt + Number(myString);
-console.log(typeof combo);
-
-
 // sum of numbers 1 to n
 function countingFromOneToN(num){
   let result = 0;
   for (let i = 1; i <= num; i ++){
-    result += i; 
-    // console.log(result);
-  }
+    result += i; }  
   return result
-
 }// end countingFromOneToN
 
-let answer = countingFromOneToN(5);
-// console.log(answer);
-let response = prompt("enter an integer number");
+// let response = prompt("enter an integer number"); //get input from user
+
+
+// ---------------------------------- JQUERY --------------------------------
+$( document ).ready( readyNow );
+
+function readyNow(){
+  console.log( 'Welcome to JQ');
+}
