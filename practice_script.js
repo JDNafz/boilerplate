6b757f54b756f38ccef7690e63d19b6007802e5c
JD Nafziger <nafziger10@gmail.com>
2023-08-17 09:46:59 -0500

    Daily problem solve complete

--- practice_script.js
+++ practice_script.js
@@ -20,8 +20,32 @@ function countingFromOneToN(num){
 
 
 // ---------------------------------- JQUERY --------------------------------
-$( document ).ready( readyNow );
+// $( document ).ready( readyNow );
 
 function readyNow(){
   console.log( 'Welcome to JQ');
 }
+
+// ------------------------------------------------------------
+function sumDigits(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    // if 3
+    if (i % 3 === 0){
+      // console.log(i);
+      sum += i;
+    }
+    // if 5
+    else if(i % 5 === 0){ // NEEDED ELSE IF 
+      sum += i;
+    };
+    // if (i % 3 === 0&& i % 5 === 0){                //bug proof of concept!
+    //   console.log(i + " is divisible by both");
+    // }
+    
+    // console.log("The sum is currently",sum,"i was",i); //used to debug
+  }
+  // console.log("conclusion sum is:",sum);
+  return sum;
+}
+// console.log(sumDigits(15));
