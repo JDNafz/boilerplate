d4857d35dbb0f6db1fe8d1be48ed2704d3e7346e
JD Nafziger <nafziger10@gmail.com>
2023-08-22 22:07:05 -0500

    cleared script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 
 
-function stringChange(string) {
-    //GOAL:
-    // "Hello World!" -> "ello world"
-    // "Ha!" -> "a"
-    // "no" -> "no"
-    let startPosition = 0;
-    let endPosition = string.length;
-    if (string.length <= 2){
-        // console.log("return:", string);
-        return string;
-    }
-    endPosition -= 1;
-    startPosition +=1
-    let newString = string.slice(startPosition,endPosition);
-    // console.log(newString);
-    return newString
 
-    //Move these down to test it
-    console.log(stringChange("Hello World!"));
-    console.log(stringChange("Ha"));
-} //end stringChange function
 
 
 
