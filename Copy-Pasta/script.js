b0f7744d206629518db65a16e8908f4c84152296
JD Nafziger <nafziger10@gmail.com>
2023-08-24 16:13:18 -0500

    JS how to access Arrays

--- Copy-Pasta/script.js
+++ Copy-Pasta/script.js
@@ -1,6 +1,23 @@
 
-$(document).ready(onReady);
 
-function onReady() {
 
-}//end onReady
+let myArray = [];
+
+let variable1 = "16";
+let variable2 = "cheese from the store down the street asldkfj lskjdfsld kfjsdlfkj lskdjf lkjsdfl kjsdfl kjsdf lkj";
+
+myArray[0] = variable1; // myArray =  [icecream ,   ,   ,   ,           ]
+//                                     0         1
+// console.log(myArray);
+
+myArray.push("tomato");
+
+
+myArray.push(variable2);
+
+console.log(myArray);
+
+console.log(myArray[0])
+
+console.log(myArray)
+
