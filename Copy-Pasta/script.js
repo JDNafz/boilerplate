de37f714e5d3ab9d7700946172ec04a2a3494943
JD Nafziger <nafziger10@gmail.com>
2023-08-24 22:02:13 -0500

    copy pasta added express urlencoder

--- Copy-Pasta/script.js
+++ Copy-Pasta/script.js
@@ -3,7 +3,7 @@
 
 let myArray = [];
 
-let variable1 = "16";
+let variable1 = 16;
 let variable2 = "cheese from the store down the street asldkfj lskjdfsld kfjsdlfkj lskdjf lkjsdfl kjsdfl kjsdf lkj";
 
 myArray[0] = variable1; // myArray =  [icecream ,   ,   ,   ,           ]
@@ -21,3 +21,20 @@ console.log(myArray[0])
 
 console.log(myArray)
 
+let number1 = 10;
+let number2 = 2;
+
+let answer = addition(number1,number2);
+console.log(answer);
+
+function addition(number1,number2){
+    let result = number1 * number2
+    return result
+}
+
+// function makeMore(number){
+
+// }
+
+// console.log(result);
+console.log(   addition(5,variable1));
--- express-cp/server/server.js
+++ express-cp/server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); //how to refer to calling the express() function
 const PORT = 5000; // Where we're chatting
 
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('server/public')); //static default to load 'public'
 
 app.get('/artist', (req, res) => {
