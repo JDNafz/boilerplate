11cedeaef1b3dd585303f5d2c1e7c1e85459dc20
JDNafz <nafziger10@gmail.com>
2023-12-13 16:02:24 -0600

    removed content from script.js

--- a.html,css,js/script.js
+++ a.html,css,js/script.js
@@ -1,27 +1,5 @@
 function testingFunction(array) {
-  console.log("Script is working", array);
 
-  const start = array[0].slice(0, 2);
-  const end = array[0].slice(2);
-  const result = [start, end];
-
-  console.log("result:", result);
-}
-
-testingFunction(["e2e4", "e7e5", "g1f3", "b8c6"]);
-
-function makeAllMoves() {
-  const updateBoard = () => {
-    if (moves !== null) {
-      let newBoard = board;
-      for (let move of moves) {
-        const start = move.slice(0, 2);
-        const end = move.slice(2);
-        newBoard = makeSimpleMove(start, end, newBoard);
-      }
-      return newBoard;
-    }
-  };
 }
 
 // Run this file in node (straight in the terminal)?
