091ab7fed799e353c0adb54ce5afe6b000860bf6
JDNafz <nafziger10@gmail.com>
2025-06-04 15:32:06 -0500

    script 'defer' added

--- a.html,css,js/script.js
+++ a.html,css,js/script.js
@@ -1,6 +1,5 @@
-function testingFunction(array) {
+	console.log("Hi, the JS is running!")
 
-}
 
 // Run this file in node (straight in the terminal)?
 //            node a.html,css,js/script.js
@@ -9,4 +8,4 @@ function testingFunction(array) {
 // MAC:       open a.html,css,js/script.js
 // Windows:   start a.html,css,js/script.js
 
-// right click inspect, go to the "Console tab to view the console.log statement"
+// right click inspect, go to the "Console tab to view the console.log statement" Refresh the page to update content. *Live Reload won't allow you to see the console.log() messages.
